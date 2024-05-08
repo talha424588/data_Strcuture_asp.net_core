513dd11d5ef61ffb4b772044b6e2550df5771f4a
Muhammad Talha <talha424588@gmail.com>
2024-05-08 11:34:01 +0500

    stack implemented

---
+++ Controllers/StackController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dsa_stack.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StackController : ControllerBase
+    {
+        [HttpGet("stack")]
+        public Dictionary<string, int> GetStack()
+        {
+            Stack<int> stack = new Stack<int>();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(3);
+            stack.Push(3);
+            stack.Pop();
+
+            Dictionary<string, int> keyValuePairs = new Dictionary<string, int>();
+            keyValuePairs.Add("count", stack.Count);
+            keyValuePairs.Add("peek", stack.Peek());
+            return keyValuePairs;
+        }
+    }
+}
