88c07bc6de28eee929632bf240aa8181b0299530
Muhammad Talha <talha424588@gmail.com>
2024-05-08 16:16:05 +0500

    stack sort array using stack

--- Controllers/StackController.cs
+++ Controllers/StackController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Http;
+
 using Microsoft.AspNetCore.Mvc;
 
 namespace dsa_stack.Controllers
@@ -8,7 +9,7 @@ namespace dsa_stack.Controllers
     public class StackController : ControllerBase
     {
         [HttpGet("stack")]
-        public Dictionary<string, int> GetStack()
+        public Dictionary<string, object> GetStack()
         {
             Stack<int> stack = new Stack<int>();
             stack.Push(1);
@@ -18,10 +19,37 @@ namespace dsa_stack.Controllers
             stack.Push(3);
             stack.Pop();
 
-            Dictionary<string, int> keyValuePairs = new Dictionary<string, int>();
+            List<int> list = stack.ToList();
+            
+
+            Dictionary<string, object> keyValuePairs = new Dictionary<string, object>();
             keyValuePairs.Add("count", stack.Count);
             keyValuePairs.Add("peek", stack.Peek());
+            keyValuePairs.Add("list", list);
             return keyValuePairs;
+        }
+
+        [HttpGet("sortStack")]
+        public List<int> sortStack()
+        {
+            int[] nums = { 1, 2, 4, 5, 63, 1, 5, 3, 2 };
+            Stack<int> stack = new Stack<int>();
+            foreach(int num in nums)
+            {
+                stack.Push(num);
+            }
+            Stack<int> storage = new Stack<int>();
+            while( stack.Count >0)
+            {
+                int tmp = stack.Pop();
+                while(storage.Count > 0  && storage.Peek() < tmp) { 
+                
+                   stack.Push(storage.Pop());
+                }
+                storage.Push(tmp);
+            }
+            List<int> list = storage.ToList();
+            return list;
         }
     }
 }
--- obj/Debug/net6.0/dsa-stack.AssemblyInfo.cs
+++ obj/Debug/net6.0/dsa-stack.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dsa-stack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+513dd11d5ef61ffb4b772044b6e2550df5771f4a")]
 [assembly: System.Reflection.AssemblyProductAttribute("dsa-stack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dsa-stack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
